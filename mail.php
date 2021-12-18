f50a5f48852a9276ca7c6541f34111255e150b26
DimasXDD <85889340+DimasXDD@users.noreply.github.com>
2021-12-18 12:26:53 +0300

    почти сделал бляяяяяяя

---
+++ mail.php
@@ -0,0 +1,24 @@
+<?php
+	require 'phpmailer/PHPMailer.php';
+	require 'phpmailer/SMTP.php';
+	require 'phpmailer/Exception.php';
+	use PHPMailer\PHPMailer\PHPMailer;
+	use PHPMailer\PHPMailer\SMTP;
+	use PHPMailer\PHPMailer\Exception;
+	$mail = new PHPMailer();
+	$mail->CharSet  = 'UTF-8';
+	$mail->isSMTP();
+	$name = $_POST['user_name'];
+	$text = $_POST['user_text'];
+	$mail->Host = "smtp.gmail.com";
+	$mail->SMTPAuth = true;
+	$mail->SMTPSecure = "tls";
+	$mail->Port = "587";
+	$mail->Username = "dimas.website69@gmail.com";
+	$mail->Password = "89805486002";
+	$mail->Subject = "Сообщение с сайта!";
+	$mail->setFrom('dimas.website69@gmail.com');
+	$mail->isHTML(true);
+	$mail->Body = "<h1>$name</h1></br><p>$text</p>";
+	$mail->addAddress('dmithry.sidnev.98@mail.ru');
+	$mail->smtpClose();
