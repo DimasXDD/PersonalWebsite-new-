11b30dc13223f3bffb9bfa1bfb2dd5ee33e8edde
DimasXDD <85889340+DimasXDD@users.noreply.github.com>
2022-01-20 19:06:15 +0300

    ебучая адаптивность аааа
    аааа

---
+++ js/scrollProgressBar.js
@@ -0,0 +1,9 @@
+window.onscroll = function(){scrollFunction()};
+
+function scrollFunction(){
+    let winScroll = document.documentElement.scrollTop;
+    let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let scrolledResult = (winScroll / height) * 100;
+    document.getElementById('progressbar').style.width = scrolledResult + "%";
+    console.log('lol')
+}
